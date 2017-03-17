95a180eebc6f4b9c700eadb8e35313354355fe0d
Brandon <Brandon@BRANDON-PC>
2017-03-17 13:58:56 -0600

    Add project files.

---
+++ Exam 1/animal.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam_1
+{
+    class animal
+    {
+        //Variables and Datatypes needed 
+        public string animalType;
+        public string animalBreed;
+        public string animalColor;
+        public string animalGender;
+        public decimal cost = 5000.00m;  //m converts it to money format
+        public decimal salesTax = 0.75m; //m converts sales tax to money format
+        public decimal calculatedTax;
+
+        //Default constructor with given default fields
+        public void Animal(string Cat, string Siamese, string Seal_Point, string Male, double cost)
+        {
+            animalType = Cat;
+            animalBreed = Siamese;
+            animalColor = Seal_Point;
+            animalGender = Male;
+            cost = 5000.00;
+        }
+
+        public void Animal()
+        {
+            
+        }
+
+        //Properties of Animal
+        public string AnimalType
+        {
+            get { return AnimalType; }
+            set { AnimalType = value; }
+        }
+
+        public string AnimalBreed
+        {
+            get { return AnimalBreed; }
+            set { AnimalBreed = value; }
+        }
+
+        public string AnimalColor
+        {
+            get { return AnimalColor; }
+            set { AnimalColor = value; }
+        }
+
+        public string AnimalGender
+        {
+            get { return AnimalGender; }
+            set { AnimalGender = value; }
+        }
+        
+        //Method that calculates the sales tax for animal purchase
+        public decimal GetCalculatedTax()
+        {
+            return calculatedTax = cost * salesTax;
+        }
+    }
+}
